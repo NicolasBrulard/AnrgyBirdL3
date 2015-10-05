2d12a10b95e321d7d9e6c6830a21a1c278202518
Thomas PERRIER <thomas.perrier@etudiant.univ-lille1.fr>
2015-10-05 14:56:52 +0200

    Y a une bouche encore mieux

--- src/controleur/Obstacle.java
+++ src/controleur/Obstacle.java
@@ -9,6 +9,7 @@ public class Obstacle {
 	private Coordonnees c; //Centre
 	private int diametre = new Random().nextInt(30)+20; // diametre entre 20 et 50
 	private Color color;
+	private int angle = -180;
 	
 	public Obstacle(Coordonnees c, Color color) {
 		this.c = c;
@@ -30,10 +31,10 @@ public class Obstacle {
 	public void draw(Graphics g) { //Dessine l'obstacle
 		g.setColor(this.color);
 		g.drawArc((int) this.c.getX()-diametre/2,(int) this.c.getY()-diametre/2, diametre, diametre, 0, 360);
-		g.setColor(Color.red);
+		g.setColor(Color.black);
 		g.drawArc((int) this.c.getX()-diametre/4, (int) this.c.getY()-diametre/4, diametre/4, diametre/4, 0, 360);
 		g.drawArc((int) this.c.getX()-diametre/4+(2*diametre/6), (int) this.c.getY()-diametre/4, diametre/4, diametre/4, 0, 360);
-		g.drawArc((int) this.c.getX()-diametre/9, (int) this.c.getY()+(diametre/20),diametre/4, diametre/4, 0, -180);
+		g.drawArc((int) this.c.getX()-diametre/9, (int) this.c.getY()+(diametre/20),diametre/4, diametre/4, 0, this.angle);
 		System.out.println();
 	}
 	
@@ -48,5 +49,13 @@ public class Obstacle {
 	
 	public Coordonnees getC() {
 		return c;
+	}
+	
+	public void setAngle(int angle) {
+		this.angle = angle;
+	}
+	
+	public int getAngle() {
+		return angle;
 	}
 }
