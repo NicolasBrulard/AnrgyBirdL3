ebfa9c2cdcd5c0eb23840ad3873c67cd032e21e1
NicolasBrulard <nicolas.brulard@free.fr>
2015-10-11 10:58:46 +0200

    Ajout trait direction
    Trait permettant de trouver la direction du bec

--- src/controleur/Bird.java
+++ src/controleur/Bird.java
@@ -14,7 +14,7 @@ import modele.Constantes;
 import modele.Coordonnees;
 
 public class Bird{ //Mon petit oiseau :D
-	private ArrayList<Coordonnees> liste = new ArrayList<>(); //Permet de récupérer tous les centres par lesquelles l'oiseau est passé
+	private ArrayList<Coordonnees> liste = new ArrayList<>(); //Permet de rï¿½cupï¿½rer tous les centres par lesquelles l'oiseau est passï¿½
 	private Coordonnees coord; //centre
 	private Color color;
 	private int coeff;
@@ -38,12 +38,12 @@ public class Bird{ //Mon petit oiseau :D
         g.drawOval((int) this.coord.getX()-Constantes.rayonBird, (int) (this.coord.getY()-Constantes.rayonBird), Constantes.rayonBird*2, Constantes.rayonBird*2);
 	}
 	
-	public void deplace(){ //Déplace les x et y selon le temps (T), Parabole codé en dur à changer en aléatoire !
+	public void deplace(){ //Dï¿½place les x et y selon le temps (T), Parabole codï¿½ en dur ï¿½ changer en alï¿½atoire !
 		this.changement(this.nbCourbe);
 		if(this.nbCourbe==0){
 		this.coord.setX(this.coeff*this.coord.getT()+this.fenetre.getWidth()/2);
 		this.coord.setY(this.coord.getT()*this.coord.getT());
-		this.coord.setT(this.coord.getT()+0.1); // Varier le + pour faire varier la fréquence de point. >0
+		this.coord.setT(this.coord.getT()+0.1); // Varier le + pour faire varier la frï¿½quence de point. >0
 		}
 		else if(this.nbCourbe==1){
 			this.coord.setX((this.coeff*Math.cos(this.coord.getT())+this.fenetre.getWidth()/2)/2);
@@ -60,7 +60,7 @@ public class Bird{ //Mon petit oiseau :D
 			this.coord.setY(50*this.coeff*Math.cos(5*this.coord.getT())*Math.cos(this.coord.getT())+this.fenetre.getWidth()/2);
 			this.coord.setT(this.coord.getT()+0.1);
 		}
-		this.liste.add(this.coord); //Permet d'ajouter la nouvelle coordonné à la liste
+		this.liste.add(this.coord); //Permet d'ajouter la nouvelle coordonnï¿½ ï¿½ la liste
 		try {
 			Thread.sleep(10);
 		} catch (Exception e) {
@@ -116,5 +116,35 @@ public class Bird{ //Mon petit oiseau :D
 		for (Coordonnees coord : liste) {
 			g.drawLine((int )coord.getX(),(int) coord.getY(), (int )coord.getX(), (int ) coord.getY());
 		}
+	}
+	
+	public void drawBec(Graphics g){
+		int x = 0;
+		int y = 0;
+		switch (this.nbCourbe) {
+		case 0:
+			x = (int) (this.coeff + this.getCoord().getX());
+			y = (int) (2* this.coord.getT()+this.getCoord().getY());
+			break;
+			
+		case 1:
+			x = (int) (-this.coeff*Math.sin(this.coord.getT()) + this.getCoord().getX());
+			y = (int) (this.coeff*Math.cos(this.coord.getT()) +this.getCoord().getY());
+			break;
+			
+		case 2:
+			x = (int) (this.coeff + this.getCoord().getX());
+			y = (int) (this.coeff*Math.cos(this.coord.getT()) +this.getCoord().getY());
+			break;
+			
+		case 3:
+			x = (int) (-this.coeff*Math.sin(this.coord.getT())*Math.cos(this.coord.getT()) + this.getCoord().getX());
+			y = (int) (this.coeff*Math.sin(this.coord.getT())*Math.sin(this.coord.getT()) +this.getCoord().getY());
+			break;
+
+		default:
+			break;
+		}
+		g.drawLine((int) this.getCoord().getX(),(int)this.getCoord().getY(),(int) x, (int) y);
 	}
 }
