2dba5b00f6b4d5683bf49a45b894a5533f23de7f
NicolasBrulard <nicolas.brulard@free.fr>
2015-12-13 17:47:12 +0100

    ..

--- src/test/TestCollisionObstacle.java
+++ src/test/TestCollisionObstacle.java
@@ -1,32 +1,35 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
+import modele.AngryBirdModele;
+import modele.Calcul;
 import modele.Coordonnees;
+import modele.CoordonneesModele;
+import modele.ObstacleModele;
 
 import org.junit.Test;
 
-import vue.AngryBird;
 import vue.FenetreContener;
-import controleur.Bird;
-import controleur.Calcul;
-import controleur.Obstacle;
 
 public class TestCollisionObstacle {
 
 	@Test
 	public void testCollisionObstacleInterieur(){
 		FenetreContener fe = new FenetreContener();
-		Bird ois = new Bird(new Coordonnees(100, 100, 0),null,fe);
-		Obstacle obs = new Obstacle(new Coordonnees(99,99,0),null);
-		ArrayList<Obstacle> ListOb = new ArrayList<Obstacle>();
+		AngryBirdModele ois = new AngryBirdModele();
+		ois.getB().setCoord(new CoordonneesModele(100, 100));
+		
+		ObstacleModele obs = new ObstacleModele(99,99);
+		ArrayList<ObstacleModele> ListOb = new ArrayList<ObstacleModele>();
 		ListOb.add(obs);
-		AngryBird angry = new AngryBird(fe);
-		angry.setBird(ois);
-		angry.setOb(ListOb);
-		assertTrue(Calcul.testContactObstacle(angry));
+		fe.setBird(ois);
+		fe.setOb(ListOb);
+		
+		assertTrue(Calcul.testContactObstacle(ois));
 	}
 	
 	@Test
